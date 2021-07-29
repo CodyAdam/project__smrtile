cde510c36f35f533e11dc38dbb410f6189c3e256
CodyAdam <cody.ad4@gmail.com>
2021-07-29 03:17:58 +0200

    Style and init browser panel

--- src/features/browser/Browser.tsx
+++ src/features/browser/Browser.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
 import styles from './Browser.module.css';
+import { TilesetCreation } from './TilesetCreation';
+
 export function Browser() {
-  return <div className={styles.container}></div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>BROWSER</h1>
+        <h2>RULES</h2>
+        <h2>BRUSHES</h2>
+        <h2>TILESETS</h2>
+        <TilesetCreation />
+      </div>
+      <div className={styles.resizeBar}></div>
+    </>
+  );
 }
