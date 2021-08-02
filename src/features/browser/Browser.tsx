a428d25600857d23aab6ddaafe749bd5eab6f05a
CodyAdam <cody.ad4@gmail.com>
2021-08-03 01:06:22 +0200

    Finish all fonctionnalities for the Browser window

--- src/features/browser/Browser.tsx
+++ src/features/browser/Browser.tsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Browser.module.css';
-import FilterIcon from '../../common/svg/Star';
 import { TilesetCreation } from './TilesetCreation';
 import SmallSquareButton from '../../common/smallSquareButton/SmallSquareButton';
 
 export function Browser() {
-  const [width, setWidth] = useState<number>(300);
+  const [showRules, setShowRules] = useState(false);
+  const [showBrushes, setShowBrushes] = useState(false);
+  const [showTilesets, setShowTilesets] = useState(false);
+  const [width, setWidth] = useState(300);
   let lastWidth = width;
   let lastPos: number = 0;
-
-  useEffect(() => {
-    console.log(width);
-  }, [width]);
 
   function handleDragStart(e: React.MouseEvent<HTMLDivElement>) {
     window.addEventListener('mousemove', handleDragMove);
@@ -32,20 +30,211 @@ export function Browser() {
   }
 
   return (
-    <div
-      className={styles.container}
-      style={{ width: `${width}px`, minWidth: `200px`, maxWidth: `${window.innerWidth - 100}px` }}
-    >
+    <div className={styles.container} style={{ width: `${width}px` }}>
       <div className={styles.title}>
         <span>BROWSER</span>
-        <SmallSquareButton onClick={() => {}} className={styles.titleButton}>
-          <FilterIcon />
+        <div className={styles.spacer} />
+        <SmallSquareButton onClick={() => {}}>
+          <div className={`icon-tag icon`} />
         </SmallSquareButton>
       </div>
-      <h2>RULES</h2>
-      <h2>BRUSHES</h2>
-      <h2>TILESETS</h2>
-      <TilesetCreation />
+      <div className={styles.scrollable}>
+        <div className={styles.group}>
+          <button
+            className={styles.groupName}
+            onClick={() => {
+              setShowRules(!showRules);
+            }}
+          >
+            <div className={styles.expandIcon}>
+              <div className={`${showRules ? 'icon-chevron-down' : 'icon-chevron-right'} icon`} />
+            </div>
+            <span>RULES</span>
+            <div className={styles.spacer} />
+            <SmallSquareButton onClick={() => {}}>
+              <div className={`icon icon-tag`} />
+            </SmallSquareButton>
+            <TilesetCreation />
+          </button>
+          <div className={styles.groupContent} hidden={!showRules}>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <button
+            className={styles.groupName}
+            onClick={() => {
+              setShowBrushes(!showBrushes);
+            }}
+          >
+            <div className={styles.expandIcon}>
+              <div className={`${showRules ? 'icon-chevron-down' : 'icon-chevron-right'} icon`} />
+            </div>
+            <span>BRUSHES</span>
+            <div className={styles.spacer} />
+            <SmallSquareButton onClick={() => {}}>
+              <div className={`icon icon-tag`} />
+            </SmallSquareButton>
+            <TilesetCreation />
+          </button>
+          <div className={styles.groupContent} hidden={!showBrushes}>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <button
+            className={styles.groupName}
+            onClick={() => {
+              setShowTilesets(!showTilesets);
+            }}
+          >
+            <div className={styles.expandIcon}>
+              <div className={`${showRules ? 'icon-chevron-down' : 'icon-chevron-right'} icon`} />
+            </div>
+            <span>TILESETS</span>
+            <div className={styles.spacer} />
+            <SmallSquareButton onClick={() => {}}>
+              <div className={`icon icon-tag`} />
+            </SmallSquareButton>
+            <TilesetCreation />
+          </button>
+          <div className={styles.groupContent} hidden={!showTilesets}>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+          </div>
+        </div>
+      </div>
       <div className={styles.resizeBar} onMouseDown={handleDragStart}></div>
     </div>
   );
