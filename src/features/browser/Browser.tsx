27a20a3643600fe852c17f7185f4159fe23e87e6
CodyAdam <cody.ad4@gmail.com>
2021-08-03 23:10:12 +0200

    Created the 2 other groups : SmartTiles and Tilesets /+1
    also implement the add button with redux logic

--- src/features/browser/Browser.tsx
+++ src/features/browser/Browser.tsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import styles from './Browser.module.css';
 import SmallSquareButton from '../../common/smallSquareButton/SmallSquareButton';
-import { addRule } from './browserSlice';
+import { addRule, addSmartTile, addTileset } from './browserSlice';
 import { useAppDispatch } from '../../app/hooks';
 
 export function Browser() {
+  const dispatch = useAppDispatch();
+
   const [showRules, setShowRules] = useState(false);
-  const [showBrushes, setShowBrushes] = useState(false);
+  const [showSmartTiles, setShowSmartTiles] = useState(false);
   const [showTilesets, setShowTilesets] = useState(false);
   const [width, setWidth] = useState(300);
   let lastWidth = width;
   let lastPos: number = 0;
-  const dispatch = useAppDispatch();
 
   function handleDragStart(e: React.MouseEvent<HTMLDivElement>) {
     window.addEventListener('mousemove', handleDragMove);
@@ -68,6 +69,61 @@ export function Browser() {
             <button>AB</button>
             <button>AB</button>
             <button>AB</button>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <button
+            className={styles.groupLabel}
+            onClick={() => {
+              setShowSmartTiles(!showSmartTiles);
+            }}
+          >
+            <div className={styles.expandIcon}>
+              <div className={`${showSmartTiles ? 'icon-chevron-down' : 'icon-chevron-right'} icon`} />
+            </div>
+            <span>SMART TILES</span>
+            <div className={styles.spacer} />
+            <SmallSquareButton
+              onClick={() => {
+                dispatch(addSmartTile(nanoid()));
+              }}
+            >
+              <div className={`icon icon-add`} />
+            </SmallSquareButton>
+          </button>
+          <div className={styles.groupContent} hidden={!showSmartTiles}>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+            <button>AB</button>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <button
+            className={styles.groupLabel}
+            onClick={() => {
+              setShowTilesets(!showTilesets);
+            }}
+          >
+            <div className={styles.expandIcon}>
+              <div className={`${showTilesets ? 'icon-chevron-down' : 'icon-chevron-right'} icon`} />
+            </div>
+            <span>TILESETS</span>
+            <div className={styles.spacer} />
+            <SmallSquareButton
+              onClick={() => {
+                dispatch(addTileset(nanoid()));
+              }}
+            >
+              <div className={`icon icon-add`} />
+            </SmallSquareButton>
+          </button>
+          <div className={styles.groupContent} hidden={!showTilesets}>
+            <button>TS1</button>
+            <button>TS2</button>
+            <button>TS3</button>
+            <button>TS4</button>
           </div>
         </div>
       </div>
