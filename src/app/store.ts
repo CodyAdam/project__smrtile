3607f40ce827f657d8197521c08dbbd7e13db69e
CodyAdam <cody.ad4@gmail.com>
2021-08-03 19:42:20 +0200

    Made browserReducer undoable

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import browserReducer from '../features/browser/browserSlice';
+import undoable from 'redux-undo';
+import { BrowserHistory } from '../features/browser/browserTypes';
 
 export const store = configureStore({
   reducer: {
+    browserReducer: undoable(browserReducer, { undoType: BrowserHistory.UNDO, redoType: BrowserHistory.REDO }),
   },
 });
 
