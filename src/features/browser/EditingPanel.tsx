a7e2bfe584b5a6f72d7713b82904e818234711ff
CodyAdam <cody.ad4@gmail.com>
2021-08-03 23:07:37 +0200

    Implement selector for the current browser selection

--- src/features/browser/EditingPanel.tsx
+++ src/features/browser/EditingPanel.tsx
@@ -1,3 +1,14 @@
+import { useAppSelector } from '../../app/hooks';
+import { selectedContentSelector } from './browserSlice';
+import styles from './EditingPanel.module.css';
+
 export function EditingPanel() {
-  return <div>MAIN PANEL HERE</div>;
+  const selectedContent = useAppSelector(selectedContentSelector);
+  if (!selectedContent)
+    return (
+      <div className={`${styles.container} ${styles.placeholder}`}>
+        <p>Select an element inside the Browser or add a new element with the icon to start</p>
+      </div>
+    );
+  return <div className={styles.container}>{selectedContent?.id}</div>;
 }
