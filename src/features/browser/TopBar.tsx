3dc0a191cf39fcc1ca6699b21c636396ac0c9fa2
CodyAdam <cody.ad4@gmail.com>
2021-08-03 23:06:29 +0200

    Renamed from Toolbar to TopBar and moved to browser dir

--- src/features/browser/TopBar.tsx
+++ src/features/browser/TopBar.tsx
@@ -1,13 +1,26 @@
-import styles from './Toolbar.module.css';
+import styles from './TopBar.module.css';
 import SmallSquareButton from '../../common/smallSquareButton/SmallSquareButton';
 import { useAppDispatch } from '../../app/hooks';
 import { undo, redo } from '../browser/browserSlice';
+import { useAppSelector } from '../../app/hooks';
+import { selectedContentSelector } from './browserSlice';
 
-export function Toolbar() {
+export function TopBar() {
+  const selectedContent = useAppSelector(selectedContentSelector);
   const dispatch = useAppDispatch();
+
+  let selectionInfo = null;
+  if (selectedContent)
+    selectionInfo = (
+      <div className={styles.infoContainer}>
+        <h1>{selectedContent.name}</h1>
+        <h1>{selectedContent.id}</h1>
+      </div>
+    );
 
   return (
     <div className={styles.container}>
+      {selectionInfo}
       <div className={styles.spacer} />
 
       <SmallSquareButton
