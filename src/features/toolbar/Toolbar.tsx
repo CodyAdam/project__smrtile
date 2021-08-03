87612b64d17fe7d2458aef43ab99e49000c05b73
CodyAdam <cody.ad4@gmail.com>
2021-08-03 19:46:07 +0200

    Add undo and redo buttons

--- src/features/toolbar/Toolbar.tsx
+++ src/features/toolbar/Toolbar.tsx
@@ -1,5 +1,29 @@
 import styles from './Toolbar.module.css';
+import SmallSquareButton from '../../common/smallSquareButton/SmallSquareButton';
+import { useAppDispatch } from '../../app/hooks';
+import { undo, redo } from '../browser/browserSlice';
 
 export function Toolbar() {
-  return <div className={styles.container}></div>;
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.spacer} />
+
+      <SmallSquareButton
+        onClick={() => {
+          dispatch(undo());
+        }}
+      >
+        <div className={`icon icon-arrow-small-left`} />
+      </SmallSquareButton>
+      <SmallSquareButton
+        onClick={() => {
+          dispatch(redo());
+        }}
+      >
+        <div className={`icon icon-arrow-small-right`} />
+      </SmallSquareButton>
+    </div>
+  );
 }
