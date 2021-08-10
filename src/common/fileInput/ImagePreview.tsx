09e692227858cff10db36ad843169c2600e888c0
CodyAdam <cody.ad4@gmail.com>
2021-08-10 17:44:37 +0200

    Added a size display under the image

--- src/common/fileInput/ImagePreview.tsx
+++ src/common/fileInput/ImagePreview.tsx
@@ -1,13 +1,27 @@
 import { TilesetFilter } from '../../features/browser/browserTypes';
 import styles from './ImagePreview.module.css';
+import { useState } from 'react';
 
 export function ImagePreview({ imageUrl, filters }: { imageUrl?: string; filters?: TilesetFilter[] }) {
+  const [size, setSize] = useState<{ width: number; height: number } | null>(null);
+
+  function handleImageLoad(e: React.SyntheticEvent<HTMLImageElement>) {
+    setSize({ width: e.currentTarget.naturalWidth, height: e.currentTarget.naturalHeight });
+  }
+
   if (imageUrl) {
     const filterClasses = filters ? (filters.includes('pixelated') ? styles.pixelated : '') : '';
     return (
-      <div className={styles.container}>
-        <img className={`${styles.img} ${filterClasses}`} src={imageUrl} alt='' />
-      </div>
+      <>
+        <div className={styles.container}>
+          <img onLoad={handleImageLoad} className={`${styles.img} ${filterClasses}`} src={imageUrl} alt='' />
+        </div>
+        {size ? (
+          <div className={styles.sizeLabel}>
+            Image size: <b>{size.width}</b> x <b>{size.height}</b>
+          </div>
+        ) : null}
+      </>
     );
   } else
     return (
