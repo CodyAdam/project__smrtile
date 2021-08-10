f2859f7d3704884f8dbe3d146b2078a461396869
CodyAdam <cody.ad4@gmail.com>
2021-08-11 01:04:31 +0200

    Simplify using the size from the selection

--- src/common/fileInput/ImagePreview.tsx
+++ src/common/fileInput/ImagePreview.tsx
@@ -1,26 +1,17 @@
-import { TilesetFilter } from '../../features/browser/browserTypes';
+import { Sprite, TilesetFilter } from '../../features/browser/browserTypes';
 import styles from './ImagePreview.module.css';
-import { useState } from 'react';
 
-export function ImagePreview({ imageUrl, filters }: { imageUrl?: string; filters?: TilesetFilter[] }) {
-  const [size, setSize] = useState<{ width: number; height: number } | null>(null);
-
-  function handleImageLoad(e: React.SyntheticEvent<HTMLImageElement>) {
-    setSize({ width: e.currentTarget.naturalWidth, height: e.currentTarget.naturalHeight });
-  }
-
-  if (imageUrl) {
+export function ImagePreview({ sprite, filters }: { sprite?: Sprite | undefined; filters?: TilesetFilter[] }) {
+  if (sprite) {
     const filterClasses = filters ? (filters.includes('pixelated') ? styles.pixelated : '') : '';
     return (
       <>
         <div className={styles.container}>
-          <img onLoad={handleImageLoad} className={`${styles.img} ${filterClasses}`} src={imageUrl} alt='' />
+          <img className={`${styles.img} ${filterClasses}`} src={sprite.url} alt='' />
         </div>
-        {size ? (
-          <div className={styles.sizeLabel}>
-            Image size: <b>{size.width}</b> x <b>{size.height}</b>
-          </div>
-        ) : null}
+        <div className={styles.sizeLabel}>
+          Image size: <b>{sprite.width}</b> x <b>{sprite.height}</b>
+        </div>
       </>
     );
   } else
