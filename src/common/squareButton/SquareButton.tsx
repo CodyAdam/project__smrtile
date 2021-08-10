99a6f288213929ad1881a8707decb2a0c70d942f
CodyAdam <cody.ad4@gmail.com>
2021-08-10 17:45:52 +0200

    Added possibility to insert icon as argument

--- src/common/squareButton/SquareButton.tsx
+++ src/common/squareButton/SquareButton.tsx
@@ -3,25 +3,41 @@ import PropTypes from 'prop-types';
 
 export function SquareButton({
   onClick,
+  icon,
   children,
   className,
   title,
 }: {
   onClick?: () => void;
+  icon?: string;
   children?: PropTypes.ReactElementLike;
   className?: string;
   title: string;
 }) {
-  return (
-    <button
-      title={title}
-      className={`${styles.button} ${className}`}
-      onClick={(e: React.MouseEvent<HTMLElement>) => {
-        e.stopPropagation();
-        if (onClick) onClick();
-      }}
-    >
-      {children}
-    </button>
-  );
+  if (icon)
+    return (
+      <button
+        title={title}
+        className={`${styles.button} ${className}`}
+        onClick={(e: React.MouseEvent<HTMLElement>) => {
+          e.stopPropagation();
+          if (onClick) onClick();
+        }}
+      >
+        {icon ? <div className={`icon icon-${icon}`} /> : null}
+      </button>
+    );
+  else
+    return (
+      <button
+        title={title}
+        className={`${styles.button} ${className}`}
+        onClick={(e: React.MouseEvent<HTMLElement>) => {
+          e.stopPropagation();
+          if (onClick) onClick();
+        }}
+      >
+        {children}
+      </button>
+    );
 }
