047eeadfa53ec0a11fa36fe4d746135f358e5d3c
CodyAdam <cody.ad4@gmail.com>
2021-08-11 23:21:26 +0200

    changed folder

---
+++ src/common/imageInput/ImageInput.tsx
@@ -0,0 +1,23 @@
+import { Sprite } from '../../features/browser/browserTypes';
+import styles from './ImageInput.module.css';
+
+export function ImageInput({ onChange }: { onChange: (sprite: Sprite) => void }) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const files = e.target.files;
+    if (files && files.length) {
+      const file = files[0];
+      const url = window.URL.createObjectURL(file);
+      const image = new Image();
+      image.src = url;
+      image.onload = () => {
+        onChange({ url: url, width: image.width, height: image.height });
+      };
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <input type='file' onChange={handleChange} accept='image/png' />
+    </div>
+  );
+}
