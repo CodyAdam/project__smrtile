13b8cd4d368226ee66b8af81ed7176a1cec96497
CodyAdam <cody.ad4@gmail.com>
2021-08-11 17:51:29 +0200

    Update every changes fix not render bug

--- src/common/numberInput/NumberInput.tsx
+++ src/common/numberInput/NumberInput.tsx
@@ -1,5 +1,5 @@
 import styles from './NumberInput.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function NumberInput({
   value = 0,
@@ -19,23 +19,29 @@ export function NumberInput({
   onChange?: (number: number) => void;
 }) {
   const [localValue, setLocalValue] = useState(value);
-  const [isDragging, setIsDragging] = useState(false);
   let lastPos = 0;
+
+  useEffect(() => {
+    setLocalValue(value);
+  }, [value]);
 
   function getSanitizedValue(value: number): number {
     const clamped = Math.max(min, Math.min(max, value));
     return Math.ceil(clamped);
   }
 
-  function handleLocalChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.valueAsNumber) {
       const newValue = getSanitizedValue(e.target.valueAsNumber);
+      if (onChange) onChange(newValue);
       setLocalValue(newValue);
-    } else setLocalValue(0);
+    } else {
+      if (onChange) onChange(0);
+      setLocalValue(0);
+    }
   }
 
   function handleMouseDown(e: React.MouseEvent) {
-    setIsDragging(true);
     lastPos = e.pageX;
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('mouseup', handleMouseUp);
@@ -45,10 +51,10 @@ export function NumberInput({
   function handleMouseMove(e: MouseEvent) {
     const dist = e.pageX - lastPos;
     setLocalValue(getSanitizedValue(localValue + incrementSpeed * dist));
+    if (onChange) onChange(getSanitizedValue(localValue + incrementSpeed * dist));
   }
 
   function handleMouseUp(e: MouseEvent) {
-    setIsDragging(false);
     window.removeEventListener('mousemove', handleMouseMove);
     window.removeEventListener('mouseup', handleMouseUp);
     window.removeEventListener('mouseleave', handleMouseUp);
@@ -58,22 +64,14 @@ export function NumberInput({
     }
   }
 
-  function handleSave() {
-    if (localValue !== value && onChange && !isDragging) onChange(localValue);
-  }
-
   return (
     <div className={styles.container}>
       {title ? <div className={styles.title}>{`${title}:`}</div> : null}
       <input
-        onChange={handleLocalChange}
-        value={localValue}
+        onChange={handleChange}
+        value={localValue.toString()}
         className={`${styles.input} ${invalid ? styles.invalid : ''}`}
         type='number'
-        onMouseLeave={handleSave}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') handleSave();
-        }}
       />
       <div className={`icon icon-gripper ${styles.slider}`} onMouseDown={handleMouseDown}></div>
     </div>
